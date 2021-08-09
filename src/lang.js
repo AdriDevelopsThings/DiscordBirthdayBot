b7023ca2c7e1de179422b1e5a44eeb176c9d865c
AdriDevelopsThings <adrianbloober@gmail.com>
2021-08-09 23:13:41 +0200

    Fix License Header
    add vscode directory
    
    Co-Authored-By: Philipp <PplusSinfo@gmail.com>

--- src/lang.js
+++ src/lang.js
@@ -15,6 +15,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 import db from './db.js'
 import { readFile } from 'fs/promises'
 import path from 'path'

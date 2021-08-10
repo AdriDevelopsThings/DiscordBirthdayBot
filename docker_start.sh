7811a3387dec4a9e2e5950c77004f30d0d9cd4f8
AdriDevelopsThings <adrianbloober@gmail.com>
2021-08-10 13:05:03 +0200

    Fix license header

--- docker_start.sh
+++ docker_start.sh
@@ -1,13 +1,3 @@
-__='
-   This is the default license template.
-   
-   File: docker_start.sh
-   Author: AdriBloober
-   Copyright (c) 2021 AdriBloober
-   
-   To edit this license information: Press Ctrl+Shift+P and press 'Create new License Template...'.
-'
-
 #!/bin/bash
 set -e
 yarn run knex --env production migrate:latest

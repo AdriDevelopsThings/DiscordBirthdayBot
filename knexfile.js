f9292b581aa7e69eaf4d080387f5657f4b21141b
AdriDevelopsThings <adrianbloober@gmail.com>
2022-10-25 20:24:59 +0200

    replace dev sqlite3 by mysql

--- knexfile.js
+++ knexfile.js
@@ -19,9 +19,13 @@
 export default {
 
   development: {
-    client: 'sqlite3',
+    client: 'mysql2',
     connection: {
-      filename: './dev.sqlite3',
+      host:     process.env.MYSQL_HOST || '127.0.0.1',
+      port:     process.env.MYSQL_PORT || '3306',
+      database: process.env.MYSQL_DATABASE || 'birthdaybot',
+      user:     process.env.MYSQL_USER || 'root',
+      password: process.env.MYSQL_PASSWORD || 'root',
     },
   },
 
